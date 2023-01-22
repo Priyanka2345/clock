c3230d3714239d0ccb8a883f34fcca0b056d7079
Priyanka2345 <iampriyanka55@gmail.com>
2023-01-22 17:02:52 +0100

    update

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,37 +1,23 @@
-// import { clear } from '@testing-library/user-event/dist/clear';
-// import { useState, useEffect } from 'react';
-// function Clock(){
-//   const [date, setDate] = useState(new Date());
-//   function refreshClock(){
-//     setDate(new Date());
-//   }
-//   useEffect(()=>{
-//     const timerId = setInterval(refreshClock, 1000);
-//     return function cleanup(){
-//       clearInterval(timerId);
-//     };
-//   },[]);
-//   return (
-//     <span>
-//       {date.toLocaleTimeString()}
-//     </span>
-//   );
-// }
-// export default Clock
-import Digit from './Digit';
-import padLeft from '../utils/padLeft';
-import useTime from '../hooks/useTime';
-
+import { clear } from '@testing-library/user-event/dist/clear';
+import { useState, useEffect } from 'react';
 function Clock(){
-  const time = useTime()
+  const [date, setDate] = useState(new Date());
+  function refreshClock(){
+    setDate(new Date());
+  }
+  useEffect(()=>{
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup(){
+      clearInterval(timerId);
+    };
+  },[]);
   return (
-    <>
-      <Digit value={padLeft(time.getSeconds()) }/>
-    </>
+    <span>
+      {date.toLocaleTimeString()}
+    </span>
   );
 }
 export default Clock
-
 
 
 
