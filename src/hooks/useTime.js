df74c3c525cb776d59b46cd18084f33c1f54fe62
Priyanka2345 <iampriyanka55@gmail.com>
2023-01-22 16:43:14 +0100

    update

--- src/hooks/useTime.js
+++ src/hooks/useTime.js
@@ -1,21 +1,16 @@
-// import { useState, useEffect } from "react";
-// export default function useTime() {
-//     const [time,setDate] = useState(new Date());
-//     function refreshClock(){
-//       setDate(new Date());
-//     }
-//     useEffect(()=>{
-//       const timerId = setInterval(refreshClock, 1000);
-//       return function cleanup(){
-//         clearInterval(timerId);
-//       };
-//     },[]);
+import { useState, useEffect } from "react";
+export default function useTime() {
+    const [time,setDate] = useState(new Date());
+    function refreshClock(){
+      setDate(new Date());
+    }
+    useEffect(()=>{
+      const timerId = setInterval(refreshClock, 1000);
+      return function cleanup(){
+        clearInterval(timerId);
+      };
+    },[]);
   
-//     return time
-//   }
+    return time
+  }
 
-import { useSeconds } from "react";
-const time =() =>{
-  const[date] = useSeconds();
-  return time;
-}
