ef68a25c75d174da206597a1ce0332c44988b3bf
Priyanka2345 <iampriyanka55@gmail.com>
2023-01-22 16:44:32 +0100

    update

--- src/components/Second.js
+++ src/components/Second.js
@@ -1,14 +1,13 @@
 import Digit from './Digit';
 import padLeft from '../utils/padLeft';
-import useTime from '../hook/useTime';
+import useTime from '../hooks/useTime';
 
 function Second(){
-    const time = useTime()
-    return(
-        <>
-        <Digit value = {padLeft(time.getSeconds())}/>
-        </>
-    );
-
+  const time = useTime()
+  return (
+    <>
+      <Digit value={padLeft(time.getSeconds()) }/>
+    </>
+  );
 }
 export default Second
