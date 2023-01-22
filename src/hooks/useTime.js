99e8e9f1c9ad3fd5a287d7f45970a33517e577c7
Priyanka2345 <iampriyanka55@gmail.com>
2023-01-22 13:15:30 +0100

    calculate

---
+++ src/hooks/useTime.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+export default function useTime() {
+    const [time, setDate] = useState(new Date());
+    function refreshClock(){
+      setDate(new Date());
+    }
+    useEffect(()=>{
+      const timerId = setInterval(refreshClock, 1000);
+      return function cleanup(){
+        clearInterval(timerId);
+      };
+    },[]);
+  
+    return time
+  }
