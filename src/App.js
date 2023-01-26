19b983178390c6d0c77468a62562cc3610403d06
Jürgen Werle <j.werle@gmx.de>
2023-01-26 20:50:41 +0100

    fix app return

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
-import Clock from "./components/Clock"
-import Second from "./components/Second"
+import Clock from "./components/Clock";
+import Second from "./components/Second";
 
 function App() {
-    return
-        <div>
-            <Clock/>
-            <Second/>
-        </div>
+  return (
+    <div>
+      <Clock />
+      <Second />
+    </div>
+  );
 }
 
 export default App;
